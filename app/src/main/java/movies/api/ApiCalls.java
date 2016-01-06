e4649930e6a4b1f23511734fc93afee416bca6c5
ahmed_nabil <a_ahmed_nabil@yahoo.com>
2016-01-06 02:22:13 +0200

    stage 1: add all sizes of posters to choose from

--- app/src/main/java/movies/api/ApiCalls.java
+++ app/src/main/java/movies/api/ApiCalls.java
@@ -13,23 +13,23 @@ public class ApiCalls {
 
     public static final String API_KEY = "6bf996b135aa742c858d7489e71a0347";
     public static final String BASE_IMAGE_POSTER_URL = "http://image.tmdb.org/t/p/";
-    public static final String BASE_IMAGE_WIDTH = "w342";
-    public static final String BASE_IMAGE_URL_AND_WIDTH = BASE_IMAGE_POSTER_URL + BASE_IMAGE_WIDTH;
+    public static final String BASE_IMAGE_WIDTH_92 = "w92";
+    public static final String BASE_IMAGE_WIDTH_154 = "w154";
+    public static final String BASE_IMAGE_WIDTH_185 = "w185";
+    public static final String BASE_IMAGE_WIDTH_342 = "w342";
+    public static final String BASE_IMAGE_WIDTH_500 = "w500";
+    public static final String BASE_IMAGE_WIDTH_780 = "w780";
+    public static final String BASE_IMAGE_WIDTH_original = "original";
+    public static final String BASE_IMAGE_URL_AND_WIDTH = BASE_IMAGE_POSTER_URL + BASE_IMAGE_WIDTH_342;
     public static final String BASE_API_CALL = "https://api.themoviedb.org/3/discover/movie?api_key="
             + API_KEY + "&sort_by=";
 
-    public static String getMovies( Context context) {
+    public static String getMovies(Context context) {
 
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
         String order = pref.getString(context.getString(R.string.pref_movies_key),
                 context.getString(R.string.pref_movies_label_popular_entry));
 
-//        if (sorting == 1)
-//            return BASE_API_CALL + "popularity.desc";
-//        if (sorting == 2)
-//            return BASE_API_CALL + "vote_average.desc";
-//
-//        return BASE_API_CALL + "popularity.desc";
         return BASE_API_CALL + order;
     }
 
