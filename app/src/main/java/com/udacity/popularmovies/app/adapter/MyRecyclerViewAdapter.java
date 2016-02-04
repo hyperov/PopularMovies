88ffe3a9ff781a2cb559b22878299b38fed7e8f8
ahmed_nabil <a_ahmed_nabil@yahoo.com>
2016-02-04 04:47:35 +0200

    still solving adapter problem

--- app/src/main/java/com/udacity/popularmovies/app/adapter/MyRecyclerViewAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.udacity.popularmovies.app.adapter;
-
-import android.support.v7.widget.RecyclerView;
-
-/**
- * Created by DELL I7 on 2/4/2016.
- */
-public class MyRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-}
