fe11b20df2938c55437071d8563da8db83bcaf0c
ahmed_nabil <a_ahmed_nabil@yahoo.com>
2016-07-27 21:59:11 +0200

    -update url of movies,trailers and reviews

--- app/src/main/java/com/udacity/popularmovies/app/api/ApiCalls.java
+++ app/src/main/java/com/udacity/popularmovies/app/api/ApiCalls.java
@@ -27,13 +27,14 @@ public class ApiCalls {
 
     public static final String BASE_IMAGE_URL_AND_WIDTH = BASE_IMAGE_POSTER_URL + BASE_IMAGE_WIDTH_185;
 
-    public static final String BASE_API_CALL_MOVIE = "https://api.themoviedb.org/3/discover/movie?api_key="
-            + API_KEY + "&sort_by=";
+//    public static final String BASE_API_CALL_MOVIE = "https://api.themoviedb.org/3/movie/" +
+//            "?api_key="
+//            + API_KEY + "&sort_by=";
 
     //use it for api call for reviews and trailers
     public static String API_CALL_MOVIE_ID;
 
-    public static final String BASE_TRAILERS_REVIEWS = "https://api.themoviedb.org/3/movie/";
+    public static final String BASE_TRAILERS_REVIEWS_MOVIES = "https://api.themoviedb.org/3/movie/";
 
     //add this string before trailer url
     public static final String YOUTUBE_BASE_URL = "https://www.youtube.com/watch?v=";
@@ -53,7 +54,7 @@ public class ApiCalls {
 
 
     public static String getApiCallReviews() {
-        return BASE_TRAILERS_REVIEWS + API_CALL_MOVIE_ID +
+        return BASE_TRAILERS_REVIEWS_MOVIES + API_CALL_MOVIE_ID +
                 "/reviews?api_key=" + API_KEY;
     }
 
@@ -61,7 +62,7 @@ public class ApiCalls {
      * @return url of reviews
      */
     public static String getApiCallTrailers() {
-        return BASE_TRAILERS_REVIEWS + API_CALL_MOVIE_ID +
+        return BASE_TRAILERS_REVIEWS_MOVIES + API_CALL_MOVIE_ID +
                 "/videos?api_key=" + API_KEY;
     }
 
@@ -72,7 +73,7 @@ public class ApiCalls {
     public static String getMovies(Context context) {
 
         if (getSettings(context) != context.getString(R.string.pref_movies_label_fav))
-            return BASE_API_CALL_MOVIE + getSettings(context);
+            return BASE_TRAILERS_REVIEWS_MOVIES + getSettings(context)+"?api_key="+API_KEY;
 
         //favourites
         return null;
